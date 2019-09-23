cfc69d4f8d9144e1bedbea524a77d655f4c53996
Roudet <Celine.Roudet@u-bourgogne.fr>
2019-09-23 09:24:55 +0200

    Ajout main dans classe Orange

--- Orange.java
+++ Orange.java
@@ -14,14 +14,15 @@ public class Orange {
     
     public Orange(double prix, String origine) 
     {
-	if(origine.equals("")){
-            this.prix=prix;
-            this.origine="Espagne";  //Espagne par défaut
-        }  
-	else{
-            this.prix=prix;
-            this.origine=origine;  
-        }   
+	if(prix < 0)
+	    this.prix = -prix;
+	else
+	    this.prix = prix;
+
+	if(origine.equals(""))
+            this.origine = "Espagne";  //Espagne par défaut
+	else
+            this.origine = origine;   
     }
 
     public double getPrix(){
@@ -35,4 +36,21 @@ public class Orange {
     public void setOrigine(String country){
 	this.origine=origine;
     }
+
+    @Override
+    public String toString(){
+        return "Orange de " + origine + " coutant " + prix + " euros";
+    }
+
+    public static void main (String[] args){
+        Orange o1 = new Orange(0.50,"France");
+        Orange o2 = new Orange(0.60,"Italie");
+        Orange o3 = new Orange(-0.40,"Espagne");
+	Orange o4 = new Orange();
+
+	System.out.println(o1);
+	System.out.println(o2);
+	System.out.println(o3);
+	System.out.println(o4);
+   }
 }
